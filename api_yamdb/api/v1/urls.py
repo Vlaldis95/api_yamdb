70715d127020bcc600b07f17862375f15642866c
Владислав Лукьяненко <vlaldis@yandex.ru>
2023-07-05 15:22:41 +0300

    Сохранены изменения в ветку develop

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -12,7 +12,7 @@ router.register('titles', TitleViewSet, basename='titles')
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
 router.register(r'titles/(?P<title_id>/d+)/reviews/',
-                   ReviewViewSet, basename='reviews')
+                ReviewViewSet, basename='reviews')
 router.register(
     r'titles/(?P<title_id>[^/.]+)/reviews/(?P<review_id>/d+)/comments',
     CommentViewSet, basename='comments')
