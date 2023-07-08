2bfd1a4620b96716881f95cc1bb099c8c2f5481f
Владислав Лукьяненко <vlaldis@yandex.ru>
2023-07-08 17:45:01 +0300

    Исправлены общие замечания

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework import serializers
 from rest_framework.exceptions import ParseError
+
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
